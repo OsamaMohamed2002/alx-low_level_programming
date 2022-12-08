0735be20e6ff810844dc73e05afd8ac7785e852d
OsamaMohamed2002 <omohame594@gmail.com>
2022-12-08 14:15:04 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	printf("Size of a cher: %lu byte($)\n", sizeof(char));
+	printf("Size of a char: %lu byte($)\n", sizeof(char));
 	printf("Size of an int: %lu byte($)\n", sizeof(int));
 	printf("Size of a long int: %lu byte($)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte($)\n", sizeof(long long int));
